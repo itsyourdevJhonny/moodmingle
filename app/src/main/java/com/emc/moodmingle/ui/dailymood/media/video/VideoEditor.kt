345a81b8e9dccebed75c2af9fa80a8b15bc416be
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-10 01:42:25 +0800

    Refactor VideoEditor to remove unused import

--- app/src/main/java/com/emc/moodmingle/ui/dailymood/media/video/VideoEditor.kt
+++ app/src/main/java/com/emc/moodmingle/ui/dailymood/media/video/VideoEditor.kt
@@ -32,7 +32,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer

6fd65bfe10101f5938d7a09462ee2e095fd94e43
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-09 17:56:22 +0800

    Allow styling of AnnotatedHashtag component

--- app/src/main/java/com/emc/moodmingle/utils/components/AnnotatedMention.kt
+++ app/src/main/java/com/emc/moodmingle/utils/components/AnnotatedMention.kt
@@ -4,11 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.text.TextStyle
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.emc.moodmingle.ui.theme.Typography
 import com.emc.moodmingle.viewmodel.firebase.FirebaseUserViewModel
 
 @Composable
-fun AnnotatedMention(mentions: List<String>) {
+fun AnnotatedMention(mentions: List<String>, style: TextStyle = Typography.bodySmall) {
     if (mentions.isNotEmpty()) {
         val userViewModel = hiltViewModel<FirebaseUserViewModel>()
 
@@ -22,6 +24,7 @@ fun AnnotatedMention(mentions: List<String>) {
 
         ExpandableAnnotatedText(
             fullText = mentionUsernames.joinToString(", ") { "@$it" },
+            style = style,
             minLines = 1
         )
     }

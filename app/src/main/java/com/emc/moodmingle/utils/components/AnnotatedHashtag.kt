123b1d9c0164cbbb8c90ee5e2798cb686c0df4be
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-09 17:56:06 +0800

    Allow styling of AnnotatedHashtag component

--- app/src/main/java/com/emc/moodmingle/utils/components/AnnotatedHashtag.kt
+++ app/src/main/java/com/emc/moodmingle/utils/components/AnnotatedHashtag.kt
@@ -1,15 +1,18 @@
 package com.emc.moodmingle.utils.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
 import com.emc.moodmingle.ui.create.post.hashtag.extractHashtags
+import com.emc.moodmingle.ui.theme.Typography
 
 @Composable
-fun AnnotatedHashtag(hashtag: String) {
+fun AnnotatedHashtag(hashtag: String, style: TextStyle = Typography.bodySmall) {
     if (hashtag.isNotBlank()) {
         val hashtags = extractHashtags(hashtag)
 
         ExpandableAnnotatedText(
             fullText = hashtags.joinToString(" ") { "#${it.replace(" ", "")}" },
+            style = style,
             minLines = 1
         )
     }

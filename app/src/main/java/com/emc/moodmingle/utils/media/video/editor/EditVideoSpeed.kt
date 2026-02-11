1f76a0a2be57ab849884a2f836145e737cf8d4bf
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-11 17:30:17 +0800

    Refactor `EditVideoSpeed` slider composables

--- app/src/main/java/com/emc/moodmingle/utils/media/video/editor/EditVideoSpeed.kt
+++ app/src/main/java/com/emc/moodmingle/utils/media/video/editor/EditVideoSpeed.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.SliderState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -26,6 +27,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.media3.session.R
 import com.emc.moodmingle.ui.theme.SecondaryDark
 import com.emc.moodmingle.ui.theme.Typography
 
@@ -61,65 +63,74 @@ fun EditVideoSpeed(state: VideoEditorState, onStateChanged: (VideoEditorState) -
                 )
             ) {
                 Icon(
-                    painter = painterResource(androidx.media3.session.R.drawable.media3_icon_rewind),
+                    painter = painterResource(R.drawable.media3_icon_rewind),
                     contentDescription = null
                 )
                 Text(text = " Reset")
             }
         }
 
-        Slider(
-            value = state.speed,
-            valueRange = 0.5f..10.0f,
-            onValueChange = { onStateChanged(state.copy(speed = it)) },
-            colors = SliderDefaults.colors(
-                activeTrackColor = SecondaryDark,
-                inactiveTrackColor = Color.Gray.copy(alpha = 0.8f),
-                thumbColor = Color.White
-            ),
-            track = { sliderState ->
-                Box {
-                    SliderDefaults.Track(
-                        sliderState = sliderState,
-                        thumbTrackGapSize = 0.dp,
-                        colors = SliderDefaults.colors(
-                            activeTrackColor = Color.Gray.copy(alpha = 0.4f),
-                            inactiveTrackColor = SecondaryDark,
-                        )
-                    )
-
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .align(Alignment.TopCenter)
-                            .offset(y = (-22).dp),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        listOf(1, 5, 10).forEach { speed ->
-                            Box(
-                                modifier = Modifier
-                                    .background(Color.Black.copy(alpha = 0.3f), CircleShape)
-                                    .padding(horizontal = 4.dp)
-                            ) {
-                                Text(
-                                    text = "${speed}x",
-                                    style = Typography.bodyMedium,
-                                    color = Color.White
-                                )
-                            }
-                        }
-                    }
-                }
-            },
-            thumb = { state ->
-                Icon(
-                    painter = painterResource(androidx.media3.session.R.drawable.media3_icon_playback_speed),
-                    contentDescription = null,
-                    tint = Color.White,
-                    modifier = Modifier.size(32.dp)
-                )
-            },
-            modifier = Modifier.background(Color.Black.copy(alpha = 0.3f), CircleShape)
-        )
+        SpeedSlider(state, onStateChanged)
     }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun SpeedSlider(state: VideoEditorState, onStateChanged: (VideoEditorState) -> Unit) {
+    Slider(
+        value = state.speed,
+        valueRange = 0.5f..10.0f,
+        onValueChange = { onStateChanged(VideoEditorState(speed = it)) },
+        colors = SliderDefaults.colors(
+            activeTrackColor = SecondaryDark,
+            inactiveTrackColor = Color.Gray.copy(alpha = 0.8f),
+            thumbColor = Color.White
+        ),
+        track = { sliderState -> SliderTrack(sliderState) },
+        thumb = { SliderThumb() },
+        modifier = Modifier.background(Color.Black.copy(alpha = 0.3f), CircleShape)
+    )
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun SliderTrack(sliderState: SliderState) {
+    Box {
+        SliderDefaults.Track(
+            sliderState = sliderState,
+            thumbTrackGapSize = 0.dp,
+            colors = SliderDefaults.colors(
+                activeTrackColor = Color.Gray.copy(alpha = 0.4f),
+                inactiveTrackColor = SecondaryDark,
+            )
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
+                .offset(y = (-22).dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            listOf(1, 5, 10).forEach { speed ->
+                Box(
+                    modifier = Modifier
+                        .background(Color.Black.copy(alpha = 0.3f), CircleShape)
+                        .padding(horizontal = 4.dp)
+                ) {
+                    Text(text = "${speed}x", style = Typography.bodyMedium, color = Color.White)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun SliderThumb() {
+    Icon(
+        painter = painterResource(R.drawable.media3_icon_playback_speed),
+        contentDescription = null,
+        tint = Color.White,
+        modifier = Modifier.size(32.dp)
+    )
 }

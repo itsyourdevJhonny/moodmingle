9f40272ad4e0f2ca365b6a97f9ccf9e002e2ec3f
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-11 17:25:37 +0800

    Create VideoEditorState data class

---
+++ app/src/main/java/com/emc/moodmingle/utils/media/video/editor/VideoEditorState.kt
@@ -0,0 +1,10 @@
+package com.emc.moodmingle.utils.media.video.editor
+
+data class VideoEditorState(
+    val startMs: Long = 0L,
+    val endMs: Long = 0L,
+    val durationMs: Long = 0L,
+    val speed: Float = 1f,
+    val volume: Float = 1f,
+    val rotation: Float = 360f
+)

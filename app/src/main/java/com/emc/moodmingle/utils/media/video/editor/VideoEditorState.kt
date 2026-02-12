87842f558728ade18baa0f752a141864dfc4dc1a
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-12 16:24:05 +0800

    Refactor: Remove unused rotation property from VideoEditorState

--- app/src/main/java/com/emc/moodmingle/utils/media/video/editor/VideoEditorState.kt
+++ app/src/main/java/com/emc/moodmingle/utils/media/video/editor/VideoEditorState.kt
@@ -9,6 +9,5 @@ data class VideoEditorState(
     val endMs: Long = 0L,
     val durationMs: Long = 0L,
     val speed: Float = 1f,
-    val volume: Float = 1f,
-    val rotation: Float = 360f
+    val volume: Float = 1f
 ) : Parcelable

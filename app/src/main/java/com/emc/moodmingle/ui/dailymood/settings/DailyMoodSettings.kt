063909eb87ef37995746b7d477507e8087d2a0e3
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-13 14:46:30 +0800

    Implement Scaffold in Daily Mood Settings

--- app/src/main/java/com/emc/moodmingle/ui/dailymood/settings/DailyMoodSettings.kt
+++ app/src/main/java/com/emc/moodmingle/ui/dailymood/settings/DailyMoodSettings.kt
@@ -1,8 +1,14 @@
 package com.emc.moodmingle.ui.dailymood.settings
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.emc.moodmingle.data.firebase.model.post.dailymood.DailyMoodEntity
 import com.emc.moodmingle.data.firebase.model.post.dailymood.DailyMoodSettings
+import com.emc.moodmingle.utils.components.ScaffoldHeader
 
 @Composable
 fun DailyMoodSettings(
@@ -10,5 +16,22 @@ fun DailyMoodSettings(
     onSettingsEdited: (DailyMoodSettings) -> Unit,
     onDismiss: () -> Unit,
 ) {
+    Scaffold(
+        topBar = {
+            ScaffoldHeader(title = "Story Settings") {
+                onDismiss()
+            }
+        }
+    ) { paddingValues ->
+        Content(paddingValues)
+    }
+}
 
+@Composable
+private fun Content(paddingValues: PaddingValues) {
+    Column(
+        modifier = Modifier.padding(paddingValues)
+    ) {
+
+    }
 }

d9ed4fe47371c16a27d13a2f511f68ab38567f65
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-13 14:40:22 +0800

    feat: Implement DailyMoodSettings composable

---
+++ app/src/main/java/com/emc/moodmingle/ui/dailymood/settings/DailyMoodSettings.kt
@@ -0,0 +1,9 @@
+package com.emc.moodmingle.ui.dailymood.settings
+
+import androidx.compose.runtime.Composable
+import com.emc.moodmingle.data.firebase.model.post.dailymood.DailyMoodEntity
+import com.emc.moodmingle.data.firebase.model.post.dailymood.DailyMoodSettings
+
+@Composable
+fun DailyMoodSettings(mood: DailyMoodEntity, onSettingsEdited: (DailyMoodSettings) -> Unit) {
+}

49420d44c593084e81b6987768aefeec52c2a040
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-16 17:02:17 +0800

    Wrap DailyMoodManualDialog in Dialog
    The `DateTimePicker` within `DailyMoodManualDialog` is now wrapped in a `Dialog` composable. This change sets `usePlatformDefaultWidth` to `false` in `DialogProperties` to allow for custom width management.

--- app/src/main/java/com/emc/moodmingle/ui/dailymood/settings/timing/DailyMoodManualDialog.kt
+++ app/src/main/java/com/emc/moodmingle/ui/dailymood/settings/timing/DailyMoodManualDialog.kt
@@ -1,16 +1,22 @@
 package com.emc.moodmingle.ui.dailymood.settings.timing
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.emc.moodmingle.utils.components.DateTimePicker
 import java.time.LocalDate
 
 @Composable
 fun DailyMoodManualDialog(onManualCreated: (LocalDate?, Long?) -> Unit, onDismiss: () -> Unit) {
-
-    DateTimePicker(
-        title = "Manual Timing",
-        doneLabel = "Save",
-        onCreated = { onManualCreated(it.date, it.time) },
-        onDismiss = onDismiss
-    )
+    Dialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        DateTimePicker(
+            title = "Manual Timing",
+            doneLabel = "Save",
+            onCreated = { onManualCreated(it.date, it.time) },
+            onDismiss = onDismiss
+        )
+    }
 }

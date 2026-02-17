fe8d923f80782486983195e1fe8e3bc1016bdee0
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-18 00:25:41 +0800

    feat: Add `SettingsTiming` model for daily mood posts
    Introduces `SettingsTiming` data class and `TimingType` enum to manage post timing options, including immediate posting, scheduling, and manual posting.

---
+++ app/src/main/java/com/emc/moodmingle/data/firebase/model/post/dailymood/settings/SettingsTiming.kt
@@ -0,0 +1,18 @@
+package com.emc.moodmingle.data.firebase.model.post.dailymood.settings
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+@Parcelize
+data class SettingsTiming(
+    val type: TimingType = TimingType.AUTO_POST_NOW,
+    val scheduledAt: LocalDateTime? = null
+) : Parcelable
+
+enum class TimingType {
+    AUTO_POST_NOW,
+    SCHEDULE,
+    MANUAL_ONLY
+}

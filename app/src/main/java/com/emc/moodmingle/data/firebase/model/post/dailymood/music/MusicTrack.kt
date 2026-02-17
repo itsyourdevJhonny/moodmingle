6483e6a027f9814cb116736fe05a8ae2bc321ba0
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-18 00:22:27 +0800

    Create MusicTrack data class

---
+++ app/src/main/java/com/emc/moodmingle/data/firebase/model/post/dailymood/music/MusicTrack.kt
@@ -0,0 +1,15 @@
+package com.emc.moodmingle.data.firebase.model.post.dailymood.music
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class MusicTrack(
+    val id: Long,
+    val title: String,
+    val artist: String,
+    @Json(name = "artworkUrl") val artworkUrl: String?,
+    @Json(name = "streamUrl") val streamUrl: String?,
+    val duration: Long,
+) : Parcelable

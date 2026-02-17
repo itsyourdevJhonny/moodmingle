f29baee348e8dc49bbd796c3a108cc81778a99b5
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-17 14:23:25 +0800

    Implement 'View List Visibility' toggle
    Adds a `SwitchButton` to the `ViewListVisibility` screen. This allows users to toggle the `viewListEnabled` setting, which is passed in and updated via callbacks.

--- app/src/main/java/com/emc/moodmingle/ui/dailymood/settings/viewlist/ViewListVisibility.kt
+++ app/src/main/java/com/emc/moodmingle/ui/dailymood/settings/viewlist/ViewListVisibility.kt
@@ -6,22 +6,36 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.emc.moodmingle.data.firebase.model.post.dailymood.DailyMoodSettings
 import com.emc.moodmingle.utils.components.ScaffoldHeader
+import com.emc.moodmingle.utils.components.SwitchButton
 
 @Composable
-fun ViewListVisibility(onDismiss: () -> Unit) {
+fun ViewListVisibility(
+    settings: DailyMoodSettings,
+    onSettingsEdited: (DailyMoodSettings) -> Unit,
+    onDismiss: () -> Unit,
+) {
     Scaffold(
         topBar = { ScaffoldHeader(title = "View List Visibility") { onDismiss() } }
     ) { paddingValues ->
-        Content(paddingValues)
+        Content(paddingValues, settings, onSettingsEdited)
     }
 }
 
 @Composable
-fun Content(paddingValues: PaddingValues) {
+fun Content(
+    paddingValues: PaddingValues,
+    settings: DailyMoodSettings,
+    onSettingsEdited: (DailyMoodSettings) -> Unit,
+) {
     Column(
         modifier = Modifier.padding(paddingValues)
     ) {
-
+        SwitchButton(
+            label = "View List Visibility",
+            isChecked = settings.viewListEnabled,
+            onCheckedChange = { onSettingsEdited(settings.copy(viewListEnabled = it)) }
+        )
     }
 }

b9a416fe7fbe60b3c2166866fcc3d56513a6d264
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-18 00:21:32 +0800

    Refactor Gif and GifType import paths

--- app/src/main/java/com/emc/moodmingle/utils/components/GifPicker.kt
+++ app/src/main/java/com/emc/moodmingle/utils/components/GifPicker.kt
@@ -47,8 +47,8 @@ import androidx.media3.ui.PlayerView
 import coil.compose.AsyncImage
 import com.emc.moodmingle.api.giphy.GiphyViewModel
 import com.emc.moodmingle.data.firebase.model.post.dailymood.DailyMoodEntity
-import com.emc.moodmingle.data.firebase.model.post.dailymood.Gif
-import com.emc.moodmingle.data.firebase.model.post.dailymood.GifType
+import com.emc.moodmingle.data.firebase.model.post.dailymood.gif.Gif
+import com.emc.moodmingle.data.firebase.model.post.dailymood.gif.GifType
 
 @Composable
 fun GifPicker(mood: DailyMoodEntity, onEdited: (DailyMoodEntity) -> Unit, onDismiss: () -> Unit) {

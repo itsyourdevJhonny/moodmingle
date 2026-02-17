25142385279aa3efbf15c7aaf166c2c6656d41fc
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-17 14:33:31 +0800

    Enhance ViewListVisibility screen UI
    - Change the switch label to dynamically show "Enabled" or "Disabled".
    - Add a descriptive text that explains the current state of the visibility setting.
    - Animate the content size of the descriptive text.
    - Add a horizontal divider for better visual separation.
    - Set the container color to black.

--- app/src/main/java/com/emc/moodmingle/ui/dailymood/settings/viewlist/ViewListVisibility.kt
+++ app/src/main/java/com/emc/moodmingle/ui/dailymood/settings/viewlist/ViewListVisibility.kt
@@ -1,12 +1,19 @@
 package com.emc.moodmingle.ui.dailymood.settings.viewlist
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.emc.moodmingle.data.firebase.model.post.dailymood.DailyMoodSettings
+import com.emc.moodmingle.ui.theme.GrayTextColor
+import com.emc.moodmingle.ui.theme.Typography
 import com.emc.moodmingle.utils.components.ScaffoldHeader
 import com.emc.moodmingle.utils.components.SwitchButton
 
@@ -17,6 +24,7 @@ fun ViewListVisibility(
     onDismiss: () -> Unit,
 ) {
     Scaffold(
+        containerColor = Color.Black,
         topBar = { ScaffoldHeader(title = "View List Visibility") { onDismiss() } }
     ) { paddingValues ->
         Content(paddingValues, settings, onSettingsEdited)
@@ -33,9 +41,21 @@ fun Content(
         modifier = Modifier.padding(paddingValues)
     ) {
         SwitchButton(
-            label = "View List Visibility",
+            label = if (settings.viewListEnabled) "Enabled" else "Disabled",
             isChecked = settings.viewListEnabled,
+            padding = 16.dp,
             onCheckedChange = { onSettingsEdited(settings.copy(viewListEnabled = it)) }
         )
+
+        HorizontalDivider(thickness = 0.5.dp)
+
+        Text(
+            text = if (settings.viewListEnabled) "Views will be displayed in the list." else "Views will not be displayed in the list.",
+            style = Typography.bodySmall.copy(color = GrayTextColor),
+            modifier = Modifier
+                .padding(top = 12.dp, start = 16.dp)
+                .animateContentSize()
+        )
+
     }
 }

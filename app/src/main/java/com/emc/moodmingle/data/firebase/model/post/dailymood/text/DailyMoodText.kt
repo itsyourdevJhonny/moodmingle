8b4969926f193867d0d7e8bb0e7e07987eac1a0a
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-18 00:20:20 +0800

    feat: Introduce DailyMoodText data model
    Introduces the `DailyMoodText` data class and `TextStyle` enum. This model will represent text elements on daily mood posts, capturing properties like description, font, color, alignment, style, and positioning.

---
+++ app/src/main/java/com/emc/moodmingle/data/firebase/model/post/dailymood/text/DailyMoodText.kt
@@ -0,0 +1,28 @@
+package com.emc.moodmingle.data.firebase.model.post.dailymood.text
+
+import android.os.Parcelable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import com.emc.moodmingle.utils.color.toHex
+import com.emc.moodmingle.utils.text.toFontName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class DailyMoodText(
+    val description: String = "",
+    val font: String = FontFamily.Default.toFontName(),
+    val color: String = Color.White.toHex(),
+    val align: String = TextAlign.Unspecified.toString(),
+    val style: TextStyle = TextStyle.NORMAL,
+    val hashtag: String = "",
+    val mentions: List<String> = emptyList(),
+    val offsetX: Float = 0f,
+    val offsetY: Float = 0f,
+) : Parcelable
+
+enum class TextStyle {
+    NORMAL,
+    WITH_BACKGROUND,
+    WITHOUT_BACKGROUND
+}

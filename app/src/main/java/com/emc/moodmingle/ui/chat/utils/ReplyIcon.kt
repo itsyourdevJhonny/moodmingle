df4296ab73e3b8c7b5c2c5be712d6676d93fc8a4
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-23 22:55:12 +0800

    Refactor: Update import paths for PostEntityFirebase and ChatMessage

--- app/src/main/java/com/emc/moodmingle/ui/chat/utils/ReplyIcon.kt
+++ app/src/main/java/com/emc/moodmingle/ui/chat/utils/ReplyIcon.kt
@@ -9,8 +9,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.emc.moodmingle.data.firebase.model.post.PostEntityFirebase
-import com.emc.moodmingle.data.firebase.model.chat.ChatMessage
+import com.emc.moodmingle.domain.remote.model.post.normal.PostEntityFirebase
+import com.emc.moodmingle.domain.remote.model.chat.ChatMessage
 import com.emc.moodmingle.utils.modifier.drawGradient
 
 @Composable

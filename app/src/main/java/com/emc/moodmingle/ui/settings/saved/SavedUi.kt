9efb79353933a29d30d53cfbf9b697cb1a7b8e22
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-23 22:57:39 +0800

    refactor: Update import paths in SavedUi
    Moves PostEntityFirebase, SaveEntityFirebase, and UserEntityFirebase to their new locations in the `domain.remote` package.

--- app/src/main/java/com/emc/moodmingle/ui/settings/saved/SavedUi.kt
+++ app/src/main/java/com/emc/moodmingle/ui/settings/saved/SavedUi.kt
@@ -1,8 +1,8 @@
 package com.emc.moodmingle.ui.settings.saved
 
-import com.emc.moodmingle.data.firebase.model.post.PostEntityFirebase
-import com.emc.moodmingle.data.firebase.model.saved.SaveEntityFirebase
-import com.emc.moodmingle.data.firebase.model.user.UserEntityFirebase
+import com.emc.moodmingle.domain.remote.model.post.normal.PostEntityFirebase
+import com.emc.moodmingle.domain.remote.model.saved.SaveEntityFirebase
+import com.emc.moodmingle.domain.remote.model.user.UserEntityFirebase
 
 data class SavedUi(
     val save: SaveEntityFirebase,

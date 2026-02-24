4010da31be9c83ad99a8b23a74ad47f1bfb5483b
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-24 18:45:30 +0800

    Refactor ShowProfilePicture: Extract Back and Download icons
    The `ShowProfilePicture` composable has been refactored by extracting the back and download icon functionality into their own private composable functions, `BackIcon` and `DownloadIcon`, for better code organization and readability.

--- app/src/main/java/com/emc/moodmingle/ui/profile/utils/ShowProfilePicture.kt
+++ app/src/main/java/com/emc/moodmingle/ui/profile/utils/ShowProfilePicture.kt
@@ -1,9 +1,11 @@
 package com.emc.moodmingle.ui.profile.utils
 
+import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -30,12 +32,8 @@ import com.emc.moodmingle.utils.modifier.drawGradient
 @Composable
 fun ShowProfilePicture(avatarUrl: String, onDismiss: () -> Unit) {
     val context = LocalContext.current
-    val fileName = avatarUrl.substringAfterLast("/")
 
-    Dialog(
-        onDismissRequest = onDismiss,
-        properties = dialogFullSizeProperties()
-    ) {
+    Dialog(onDismissRequest = onDismiss, properties = dialogFullSizeProperties()) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -43,45 +41,50 @@ fun ShowProfilePicture(avatarUrl: String, onDismiss: () -> Unit) {
                 .padding(bottom = 42.dp),
             contentAlignment = Alignment.Center
         ) {
-            AsyncImage(
-                model = avatarUrl,
-                contentDescription = "Avatar"
-            )
-
-            Icon(
-                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                contentDescription = "Back",
-                modifier = Modifier
-                    .align(Alignment.TopStart)
-                    .offset(x = 20.dp, y = 28.dp)
-                    .clickable { onDismiss() }
-            )
-
-            Box(
-                modifier = Modifier
-                    .padding(end = 16.dp, bottom = 20.dp)
-                    .align(Alignment.BottomEnd)
-                    .clickable {
-                        if (isImageInGallery(fileName)) {
-                            Toast.makeText(context, "Image already saved", Toast.LENGTH_SHORT)
-                                .show()
-                            return@clickable
-                        }
-
-                        downloadImage(context, avatarUrl, fileName)
-                        Toast.makeText(context, "Image saved to gallery", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.download_outlined),
-                    contentDescription = "Download",
-                    modifier = Modifier
-                        .size(32.dp)
-                        .graphicsLayer(alpha = 0.7f)
-                        .drawGradient()
-                )
-            }
+            AsyncImage(model = avatarUrl, contentDescription = "Avatar")
+            BackIcon(onDismiss)
+            DownloadIcon(context, avatarUrl)
         }
+    }
+}
+
+@Composable
+private fun BoxScope.BackIcon(onDismiss: () -> Unit) {
+    Icon(
+        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+        contentDescription = "Back",
+        modifier = Modifier
+            .align(Alignment.TopStart)
+            .offset(x = 20.dp, y = 28.dp)
+            .clickable { onDismiss() }
+    )
+}
+
+@Composable
+private fun BoxScope.DownloadIcon(context: Context, avatarUrl: String) {
+    val fileName = avatarUrl.substringAfterLast("/")
+
+    Box(
+        modifier = Modifier
+            .padding(end = 16.dp, bottom = 20.dp)
+            .align(Alignment.BottomEnd)
+            .clickable {
+                if (isImageInGallery(fileName)) {
+                    Toast.makeText(context, "Image already saved", Toast.LENGTH_SHORT).show()
+                    return@clickable
+                }
+
+                downloadImage(context, avatarUrl, fileName)
+                Toast.makeText(context, "Image saved to gallery", Toast.LENGTH_SHORT).show()
+            }
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.download_outlined),
+            contentDescription = "Download",
+            modifier = Modifier
+                .size(32.dp)
+                .graphicsLayer(alpha = 0.7f)
+                .drawGradient()
+        )
     }
 }

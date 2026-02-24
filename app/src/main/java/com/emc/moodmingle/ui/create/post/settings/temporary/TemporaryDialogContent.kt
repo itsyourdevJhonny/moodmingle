bf74dda87532681ca1c28b8dd469055d06107837
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-24 18:48:24 +0800

    refactor: Update PostTemporary import path
    Updates the import path for the `PostTemporary` model in `TemporaryDialogContent.kt` to reflect its new location in the domain layer.

--- app/src/main/java/com/emc/moodmingle/ui/create/post/settings/temporary/TemporaryDialogContent.kt
+++ app/src/main/java/com/emc/moodmingle/ui/create/post/settings/temporary/TemporaryDialogContent.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.emc.moodmingle.data.firebase.model.post.settings.PostTemporary
+import com.emc.moodmingle.domain.remote.model.post.settings.PostTemporary
 import com.emc.moodmingle.ui.theme.GrayTextColor
 import com.emc.moodmingle.ui.theme.Typography
 import com.emc.moodmingle.utils.components.SwitchButton

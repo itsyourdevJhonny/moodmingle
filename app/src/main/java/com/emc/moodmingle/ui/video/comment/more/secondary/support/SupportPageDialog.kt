cc51ec2431ecf0aa8a4fd7d27fc7eb05b2eee909
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-24 18:47:27 +0800

    Refactor: Update import paths in SupportPageDialog
    Updated the import paths for `UserEntityFirebase`, `VideoComment`, and `formatTimeAgo` to reflect their new locations. The `FirebaseUserViewModel` import path was also changed from `viewmodel.firebase` to `viewmodel.remote`.

--- app/src/main/java/com/emc/moodmingle/ui/video/comment/more/secondary/support/SupportPageDialog.kt
+++ app/src/main/java/com/emc/moodmingle/ui/video/comment/more/secondary/support/SupportPageDialog.kt
@@ -57,9 +57,9 @@ import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.emc.moodmingle.R
-import com.emc.moodmingle.data.firebase.model.user.UserEntityFirebase
-import com.emc.moodmingle.data.firebase.model.video.VideoComment
-import com.emc.moodmingle.data.model.post.formatTimeAgo
+import com.emc.moodmingle.domain.remote.model.user.UserEntityFirebase
+import com.emc.moodmingle.domain.remote.model.video.VideoComment
+import com.emc.moodmingle.domain.local.model.post.formatTimeAgo
 import com.emc.moodmingle.ui.post.text.ExpandableAutoDetectClickableText
 import com.emc.moodmingle.ui.theme.GrayTextColor
 import com.emc.moodmingle.ui.theme.PrimaryDark
@@ -70,7 +70,7 @@ import com.emc.moodmingle.ui.theme.Typography
 import com.emc.moodmingle.utils.modifier.drawGradient
 import com.emc.moodmingle.utils.modifier.gradientCircleBorder
 import com.emc.moodmingle.utils.modifier.roundedGrayBorder
-import com.emc.moodmingle.viewmodel.firebase.FirebaseUserViewModel
+import com.emc.moodmingle.viewmodel.remote.FirebaseUserViewModel
 import kotlin.random.Random
 
 private data class SupportMessageGroup(

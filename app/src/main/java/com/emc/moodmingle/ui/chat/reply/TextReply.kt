3e875f9e26be31245946e4ffbfaf95289475b036
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-24 18:51:39 +0800

    Refactor: Update ChatMessage import path

--- app/src/main/java/com/emc/moodmingle/ui/chat/reply/TextReply.kt
+++ app/src/main/java/com/emc/moodmingle/ui/chat/reply/TextReply.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.emc.moodmingle.data.firebase.model.chat.ChatMessage
+import com.emc.moodmingle.domain.remote.model.chat.ChatMessage
 import com.emc.moodmingle.ui.theme.BrushSecondaryTertiaryGradient
 import com.emc.moodmingle.ui.theme.GrayTextColor
 

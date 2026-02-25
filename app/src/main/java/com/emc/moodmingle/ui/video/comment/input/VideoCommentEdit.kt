5f0095ae304dd32aca11a5155ab25eda2e100f3e
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-25 22:43:20 +0800

    Refactor: Update VideoComment import path
    Updates the import path for `VideoComment` in `VideoCommentEdit.kt` to reflect its new location in the `domain.remote.model.video` package.

--- app/src/main/java/com/emc/moodmingle/ui/video/comment/input/VideoCommentEdit.kt
+++ app/src/main/java/com/emc/moodmingle/ui/video/comment/input/VideoCommentEdit.kt
@@ -27,7 +27,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.emc.moodmingle.data.firebase.model.video.VideoComment
+import com.emc.moodmingle.domain.remote.model.video.VideoComment
 import com.emc.moodmingle.ui.theme.GrayTextColor
 import com.emc.moodmingle.ui.theme.SecondaryDark
 import com.emc.moodmingle.ui.theme.Typography

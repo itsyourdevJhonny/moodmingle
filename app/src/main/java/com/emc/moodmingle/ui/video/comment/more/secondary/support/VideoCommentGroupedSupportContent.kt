c30e2cf09eb53f14ad87099ce25d40cb833be5cf
itsyourdevJhonny <jhonleemarahay@gmail.com>
2026-02-25 22:42:15 +0800

    Refactor: Update import paths in VideoCommentGroupedSupportContent
    Adjusts the import paths for the following to reflect their new locations:
    - `UserEntityFirebase`
    - `Support`
    - `formatTimeAgo`
    - `FirebaseUserViewModel`

--- app/src/main/java/com/emc/moodmingle/ui/video/comment/more/secondary/support/VideoCommentGroupedSupportContent.kt
+++ app/src/main/java/com/emc/moodmingle/ui/video/comment/more/secondary/support/VideoCommentGroupedSupportContent.kt
@@ -26,16 +26,16 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
-import com.emc.moodmingle.data.firebase.model.user.UserEntityFirebase
-import com.emc.moodmingle.data.firebase.model.video.Support
-import com.emc.moodmingle.data.model.post.formatTimeAgo
+import com.emc.moodmingle.domain.remote.model.user.UserEntityFirebase
+import com.emc.moodmingle.domain.remote.model.video.Support
+import com.emc.moodmingle.domain.local.model.post.formatTimeAgo
 import com.emc.moodmingle.ui.post.text.ExpandableAutoDetectClickableText
 import com.emc.moodmingle.ui.theme.GrayTextColor
 import com.emc.moodmingle.ui.theme.SecondaryDark
 import com.emc.moodmingle.ui.theme.Typography
 import com.emc.moodmingle.utils.modifier.gradientCircleBorder
 import com.emc.moodmingle.utils.modifier.roundedGrayBorder
-import com.emc.moodmingle.viewmodel.firebase.FirebaseUserViewModel
+import com.emc.moodmingle.viewmodel.remote.FirebaseUserViewModel
 
 @Composable
 fun VideoCommentGroupedSupportContent(content: Any) {
